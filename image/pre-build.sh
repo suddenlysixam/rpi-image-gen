aed106763cf0b8ea2d3dfe03041c4dd6aec7d37b
Matthew Lear <matthew.lear@raspberrypi.com>
2025-08-13 12:46:01 +0100

    hooks: remove non-functional stubs and clean up

--- image/pre-build.sh
+++ image/pre-build.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-if igconf isset image_pmap ; then
+if igconf isval image_pmap ; then
    [[ -f "${IGconf_image_assetdir}/device/provisionmap-${IGconf_image_pmap}.json" ]] || \
       die "pmap not found"
 fi
